e85fcbe2f4fba87336b2b6c37c10fb0ba80bf8d9
WilliELM <will585g@stud.kea.dk>
2023-04-19 18:48:35 +0200

    added functionality for search bar

--- app/src/main/java/com/example/filmatic_app/MainActivity.java
+++ app/src/main/java/com/example/filmatic_app/MainActivity.java
@@ -1,16 +1,56 @@
 package com.example.filmatic_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private SearchView searchView;
+    private List<MovieFetcher> itemList;
 
+
+    public void setFilteredList (List<MovieFetcher> filteredList){
+        this.itemList = filteredList;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchView = findViewById(R.id.SearchBar1);
+        searchView.clearFocus();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterList(newText);
+                return true;
+            }
 
 
+        });
+        itemList = new ArrayList<>();
+    }
+    private void filterList(String text) {
+        List<MovieFetcher> filteredList = new ArrayList<>();
+        for (MovieFetcher movie : itemList){
+            if (movie.getTitle().toLowerCase().contains(text.toLowerCase())){
+                filteredList.add(movie);
+            }
+        }
+        if (filteredList.isEmpty()){
+            Toast.makeText(this,"No data found", Toast.LENGTH_SHORT).show();
+        } else {
+
+        }
     }
 }
